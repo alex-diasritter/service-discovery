b67c2deb27be362d8a41b2b844a4928647803acb
alex-diasritter <ritter.alex@hotmail.com>
2025-09-10 20:17:20 -0300

    reescrevendo comentários

--- src/main/java/com/alex/service_discovery/ServiceDiscoveryApplication.java
+++ src/main/java/com/alex/service_discovery/ServiceDiscoveryApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 /*
-Essa aplicação simples contendo a dependencia Eureka Server  obe o servidor Spring Eureka.
+Essa aplicação contém a dependencia Eureka Server e sobe o servidor Spring Eureka.
 As outras apliações usando depen. Eureka Client.
 O Eureka fará a roteamento entre o Gateway e os microsserviços.
 gateway -> eureka -> serviços
